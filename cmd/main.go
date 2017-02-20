4ffd3dccfcda65bc2e79a8afaf5827a3074a8402
Gustavo Chain <gchain@gmail.com>
2017-02-20 08:35:32 -0300

    Parameterize port

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"time"
@@ -28,10 +30,15 @@ func NewHandler(ui *httplab.UI, g *gocui.Gui) http.Handler {
 }
 
 func main() {
+	var port int
+	flag.IntVar(&port, "port", 10080, "Specifies the port where HTTPLab will bind to")
+	flag.Parse()
+
 	g, err := gocui.NewGui(gocui.Output256)
 	if err != nil {
 		log.Fatalln(err)
 	}
+	defer g.Close()
 
 	ui := httplab.NewUI()
 	if err := ui.Init(g); err != nil {
@@ -40,8 +47,11 @@ func main() {
 
 	http.Handle("/", NewHandler(ui, g))
 	go func() {
-		ui.Info(g, "Listening on :18000")
-		if err := http.ListenAndServe(":18000", nil); err != nil {
+		// Make sure gocui has started
+		g.Execute(func(g *gocui.Gui) error { return nil })
+
+		ui.Info(g, "Listening on :%d", port)
+		if err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil); err != nil {
 			log.Fatalln(err)
 		}
 	}()
